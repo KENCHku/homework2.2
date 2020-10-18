6787cac7c770caa6f27ee1d1d35454219c94adee
KENCHku <kench.mkk3@gmail.com>
2020-10-18 14:11:20 +0600

    Initial commit

---
+++ src/Transport.java
@@ -0,0 +1,16 @@
+public class Transport implements Printable{
+    private double volume;
+
+    public Transport(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
